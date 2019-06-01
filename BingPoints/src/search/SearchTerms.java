7ee3455b061f1f77355eae6d539930f9983d9901
Collin Cresta <47609933+ccresta1386@users.noreply.github.com>
2019-06-01 12:56:07 -0400

    Added FileUtils

--- BingPoints/src/search/SearchTerms.java
+++ BingPoints/src/search/SearchTerms.java
@@ -1,11 +1,21 @@
 package search;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+import org.apache.commons.io.FileUtils;
+
+
 public class SearchTerms {
 	
 	private String[] searchTerms;
 	
 	
-	
+	public void setSearchTerms() {
+		String str = FileUtils.readFileToString("C:\\Users\\colli\\eclipse-workspace\\SignInInfo\\Resources");
+		for(int ndx = 0; ndx < )
+	}
 	
 	
 	
